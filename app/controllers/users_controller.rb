5811a0d4e1601603de00cd97c8de3243e9d1e707
Hugo Lepetit <perso@hugolepetit.fr>
2022-01-10 15:08:39 +0100

    Restrict download_attestations according to user tokens privileges

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < AuthenticatedUsersController
   def profile
     @user = current_user
+    @tokens = current_user.jwt_api_entreprise
+      .unexpired
+      .not_blacklisted
+      .where(archived: false)
+
+    @roles = @tokens.joins(:roles).pluck('roles.code').uniq
+
+    @can_download_attestations = (@roles & %w(attestations_sociales attestations_fiscales)).any?
   end
 
   def download_attestations
--- spec/features/user_attestations_spec.rb
+++ spec/features/user_attestations_spec.rb
@@ -20,11 +20,52 @@ RSpec.describe 'User can download attestations', type: :feature do
 
     let(:jwt) { user.jwt_api_entreprise.take }
 
-    it 'has a menu item for attestations download' do
-      within('.authenticated-user-sidemenu') do
-        expect(page).to have_content(I18n.t('.shared.user_signed_in_side_menu.download_attestations'))
+    let(:role_attestations_sociales) { create(:role, code: 'attestations_sociales') }
+    let(:role_attestations_fiscales) { create(:role, code: 'attestations_fiscales') }
+
+    let(:user_no_roles) { create(:user, :with_jwt) }
+
+    let(:user_both_roles) do
+      create(:user).tap do |u|
+        create(:jwt_api_entreprise, roles: [ role_attestations_sociales, role_attestations_fiscales ], user: u)
       end
     end
+
+    let(:user_sociales_only) do
+      create(:user).tap do |u|
+        create(:jwt_api_entreprise, roles: [ role_attestations_sociales], user: u)
+      end
+    end
+
+    context 'user has no roles allowing attestations consultation' do
+      let(:user) { user_no_roles }
+
+      it 'does NOT have a menu item for attestations download' do
+        within('.authenticated-user-sidemenu') do
+          expect(page).not_to have_content(I18n.t('.shared.user_signed_in_side_menu.download_attestations'))
+        end
+      end
+    end
+
+    context 'user has both roles for both attestations consultation' do
+      let(:user) { user_both_roles }
+
+      it 'does have a menu item for attestations download' do
+        within('.authenticated-user-sidemenu') do
+          expect(page).to have_content(I18n.t('.shared.user_signed_in_side_menu.download_attestations'))
+        end
+      end
+    end
+
+    context 'user has role allowing attestations sociales consultation' do
+      let(:user) { user_sociales_only }
+
+      it 'does have a menu item for attestations download' do
+        within('.authenticated-user-sidemenu') do
+          expect(page).to have_content(I18n.t('.shared.user_signed_in_side_menu.download_attestations'))
+        end
+      end
+    end
+
   end
 end
-
