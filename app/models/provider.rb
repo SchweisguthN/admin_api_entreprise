37ebb3b26b79a8e68aae94db235566fe0865cc51
Samuelfaure <samuel.faure.dev@gmail.com>
2022-06-28 12:01:27 +0200

    changed provider external link on homepage with catalogue filtered link

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,9 +1,14 @@
 class Provider
   include ActiveModel::Model
+  include Rails.application.routes.url_helpers
 
-  attr_accessor :slug, :name, :link
+  attr_accessor :slug, :name, :link_external
 
   def self.all
     I18n.t('providers').map { |entry| new(entry) }
+  end
+
+  def link_catalogue_filtered_by
+    "#{endpoints_path}?Endpoint[query]=#{slug}"
   end
 end
